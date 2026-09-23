023bccb85a239b9922303037e30d4d4e0fa7bbc9
Aslam <aslamahamadattar@gmail.com>
2026-09-23 18:32:55 +0530

    Added hamburger menu and category cards

--- script.js
+++ script.js
@@ -1,28 +1,53 @@
 let categoryList = document.getElementById("categoryList");
+let categoryCard = document.getElementById("category-card");
+let search = document.getElementById("search");
+
+let categories = [];
 
 fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
     .then((res) => res.json())
     .then((data) => {
+             categories = data.categories;
+        categories.forEach((category) => {
 
-        data.categories.forEach((category) => {
-
+            // Offcanvas category list
             categoryList.innerHTML += `
                 <a href="#" class="category">
-                    ${category.strCategory}
+                    ${category.strCategory} 
                 </a>
+                <hr>
+            `;
+
+
+            // Category cards
+            categoryCard.innerHTML += `
+                <div class="category-card">
+
+                    <a href = ""><img src="${category.strCategoryThumb}" 
+                            alt="${category.strCategory}"></a>
+
+                    <span class="category-name">
+                        ${category.strCategory}
+                    </span>
+
+                </div>
             `;
 
         });
 
-        let categories = document.querySelectorAll(".category");
 
-        categories.forEach((category) => {
+        // Offcanvas category click
+        let categoriesLinks = document.querySelectorAll(".category");
+
+        categoriesLinks.forEach((category) => {
 
             category.addEventListener("click", function (e) {
 
                 e.preventDefault();
 
                 let categoryName = this.innerText;
+
+                console.log(categoryName);
 
             });
 
@@ -33,4 +58,27 @@ fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
         console.log(error);
     });
 
+    search.addEventListener("input",()=>{
+        let searchValue = search.value.toLowerCase();
+        
+        categoryCard.innerHTML = "";
 
+        categories.forEach((category)=>{
+
+            if(category.strCategory.toLowerCase().includes(searchValue)){
+                categoryCard.innerHTML += `
+                <div class="category-card">
+
+                    <a href = ""><img src="${category.strCategoryThumb}" 
+                            alt="${category.strCategory}"></a>
+
+                    <span class="category-name">
+                        ${category.strCategory}
+                    </span>
+
+                </div>
+            `;
+
+            }
+        })
+    })
